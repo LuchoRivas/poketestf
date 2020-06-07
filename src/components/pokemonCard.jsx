7e60ea3b2a9bb64d3b6140a7605d41deb289605a
unknown <hpcarg@gmail.com>
2020-06-07 02:25:37 -0300

    sinny! detailmodal-componente

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Card, Button, Form } from "react-bootstrap";
+import DetailModal from "./detailmodal";
 
 const PokeCard = (props) => {
-    const [isShinny, setIsOpened] = React.useState(false);
+    const [isShinny, setShinny] = React.useState(false);
+    const [modalShow, setModalShow] = React.useState(false);
 
-    function toggle() {
-      setIsOpened(wasOpened => !wasOpened);
+    const toggleShinny = () => {
+        setShinny(shinny => !shinny);
     }
+
     return(
         <React.Fragment>
             {
@@ -14,7 +17,7 @@ const PokeCard = (props) => {
                 <Card style={{ width: '18rem' }} className={`pokemon-type-${props.pokemon.types[0].type.name}`}>
                     <Card.Img hidden={isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
                     <Card.Img hidden={!isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_shiny}`} />
-                    <Form.Check onClick={toggle} className="px-3" inline label="shinny" id={'shinny'} />
+                    <Form.Check onClick={toggleShinny} className="px-3" inline label="shinny" id={'shinny'} />
                     <Card.Body>
                         <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
                         <Card.Text>
@@ -22,10 +25,14 @@ const PokeCard = (props) => {
                                 props.pokemon.species[0].flavor_text
                             }
                         </Card.Text>
-                        <Button variant="primary">Ver mas</Button>
+                        <Button onClick={() => setModalShow(true)} variant="primary">Ver mas</Button>
                     </Card.Body>
                 </Card>
             }
+                <DetailModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                />
         </React.Fragment>
     )
 }

5c5099a971be02f07224f17d6f5bea40cf44d210
unknown <hpcarg@gmail.com>
2020-06-06 23:36:25 -0300

    props-thrusty

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -4,18 +4,21 @@ import { Card, Button } from "react-bootstrap";
 const PokeCard = (props) => {
     return(
         <React.Fragment>
-            <Card style={{ width: '18rem' }} className={`pokemon-type-${props.pokemon.types[0].type.name}`}>
-                <Card.Img variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
-                <Card.Body>
-                    <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
-                    <Card.Text>
-                        {
-                            props.pokemon.species[0].flavor_text
-                        }
-                    </Card.Text>
-                    <Button variant="primary">Ver mas</Button>
-                </Card.Body>
-            </Card>
+            {
+                props &&
+                <Card style={{ width: '18rem' }} className={`pokemon-type-${props.pokemon.types[0].type.name}`}>
+                    <Card.Img variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
+                    <Card.Body>
+                        <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
+                        <Card.Text>
+                            {
+                                props.pokemon.species[0].flavor_text
+                            }
+                        </Card.Text>
+                        <Button variant="primary">Ver mas</Button>
+                    </Card.Body>
+                </Card>
+            }
         </React.Fragment>
     )
 }

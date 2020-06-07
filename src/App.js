2e01acf7f021743759298f81926d5ee2b2c876a2
unknown <hpcarg@gmail.com>
2020-06-07 00:20:01 -0300

    componente-search-en-app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
 import React from 'react';
 import './App.css';
-import { Container, Button, InputGroup, FormControl, Row, Col } from "react-bootstrap";
-import { getPokemonByName } from "./services/pokemonService";
-import PokeCard from "./components/pokemonCard";
+import { Container, Row, Col } from "react-bootstrap";
+import Search from "./components/search";
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  };
-
-  state = {
-    pokemon: undefined,
-    search: undefined,
-    error: undefined
-  };
-
-  async resetState() {
-    this.setState({
-      pokemon: undefined,
-      search: undefined,
-      error: undefined
-    })
-  };
-
-  async getPokemonByName(search) {
-    this.state.error && await this.resetState();
-    try {
-        const pokemon = await getPokemonByName(search);
-        this.setState({ pokemon: pokemon });
-    }
-    catch(err) {
-      if(err.response.status === 500) {
-        const error = err.response.statusText
-        this.state.pokemon && await this.resetState();
-        this.setState({ error: error });
-      }
-    }
-  };
-
-  handleChange(event) {
-    this.setState({search: event.target.value});
-  };
 
   render(){
     return(
@@ -53,24 +15,7 @@ class App extends React.Component {
           </Row>
           <Row className="d-flex justify-content-center">
             <Col xs={6}>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <Button onClick={(search) => this.getPokemonByName(this.state.search)} variant="outline-secondary">Seach</Button>
-                </InputGroup.Prepend>
-                <FormControl aria-describedby="basic-addon1" onChange={this.handleChange} value={this.state.search}/>
-              </InputGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex justify-content-center">
-              {
-                this.state.pokemon &&
-                  <PokeCard pokemon={this.state.pokemon}></PokeCard>
-              }
-              {
-                this.state.error &&
-                  <span>{'No se encontraron resultados'}</span>
-              }
+              <Search></Search>
             </Col>
           </Row>
         </Container>

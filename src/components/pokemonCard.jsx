6db1bb44a3af3055b9dd132821d52f092419922d
unknown <hpcarg@gmail.com>
2020-06-07 17:34:20 -0300

    shiny-lol

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -3,11 +3,11 @@ import { Card, Button, Form } from "react-bootstrap";
 import DetailModal from "./detailmodal";
 
 const PokeCard = (props) => {
-    const [isShinny, setShinny] = React.useState(false);
+    const [isShiny, setShinny] = React.useState(false);
     const [modalShow, setModalShow] = React.useState(false);
 
     const toggleShinny = () => {
-        setShinny(shinny => !shinny);
+        setShiny(shiny => !shiny);
     }
 
     return(
@@ -15,8 +15,8 @@ const PokeCard = (props) => {
             {
                 props &&
                 <Card style={{ width: '18rem' }} className={`pokemon-type-${props.pokemon.types[0].type.name}`}>
-                    <Card.Img hidden={isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
-                    <Card.Img hidden={!isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_shiny}`} />
+                    <Card.Img hidden={isShiny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
+                    <Card.Img hidden={!isShiny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_shiny}`} />
                     <Form.Check onClick={toggleShinny} className="px-3" inline label="shinny" id={'shinny'} />
                     <Card.Body>
                         <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>

9353cb7777d780a979bd2e702ddcc6bb09923ad8
unknown <hpcarg@gmail.com>
2020-06-07 01:56:02 -0300

    shinny-checkbox

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
 
 const PokeCard = (props) => {
+    const [isShinny, setIsOpened] = React.useState(false);
+
+    function toggle() {
+      setIsOpened(wasOpened => !wasOpened);
+    }
     return(
         <React.Fragment>
             {
                 props &&
                 <Card style={{ width: '18rem' }} className={`pokemon-type-${props.pokemon.types[0].type.name}`}>
-                    <Card.Img variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
+                    <Card.Img hidden={isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_default}`} />
+                    <Card.Img hidden={!isShinny} variant="top" alt="pokemon_image" src={`${props.pokemon.sprites.front_shiny}`} />
+                    <Form.Check onClick={toggle} className="px-3" inline label="shinny" id={'shinny'} />
                     <Card.Body>
                         <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
                         <Card.Text>

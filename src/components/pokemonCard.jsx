48ae6c956401a7bc06f5df9b99cde88b6c8d49cf
Luis Rivas <lrivas@in.hq.binit.com.ar>
2020-06-10 00:16:43 -0300

    fixes keys color2 variaciones

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -10,20 +10,19 @@ const PokeCard = (props) => {
         setShiny(shiny => !shiny);
     }
     const color1 = props.pokemon.types[0].type.name;
-    const color2 = props.pokemon.types[1] ? props.pokemon.types[1].type.name : false;
     const imgCol = props.pokemon.sprites.front_female ? 'col-4' : 'col-6'
     return (
         <React.Fragment>
             {
                 props &&
                 <Row>
-                    <Col class="col-9">
+                    <Col xs={9}>
                         <Card className={`pokemon-type-${color1}`}>
                             <Card.Body>
                                 <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
                                 <div>{
-                                    props.pokemon.types.map(pokemonType =>
-                                        <h8 className="text-uppercase d-inline"><Badge className={`pokemon-type-${pokemonType.type.name}`} variant="dark">{pokemonType.type.name}</Badge>{' '}</h8>
+                                    props.pokemon.types.map((pokemonType, i) =>
+                                        <h6 key={`badge_${pokemonType.type.name}_${i}`} className="text-uppercase d-inline"><Badge className={`pokemon-type-${pokemonType.type.name}`} variant="dark">{pokemonType.type.name}</Badge>{' '}</h6>
                                     )
                                 }</div>
                                 <Card.Text>
@@ -54,13 +53,8 @@ const PokeCard = (props) => {
                             <Card.Body>
                                 <Card.Title className="text-capitalize">{props.pokemon.name}</Card.Title>
                                 <div>{
-                                    props.pokemon.types.map(pokemonType =>
-                                        <h8 className="text-uppercase d-inline"><Badge className={`pokemon-type-${pokemonType.type.name}`} variant="dark">{pokemonType.type.name}</Badge>{' '}</h8>
-                                    )
-                                }</div>
-                                <div><p>Variaciones:</p>{
-                                    props.pokemon.variations.varieties.map(varieties =>
-                                        <p className={`pokemon-type-${varieties.pokemon.name}`} variant="dark">{varieties.pokemon.name}</p>
+                                    props.pokemon.types.map((pokemonType, i) =>
+                                        <h6 key={`badge2_${pokemonType.type.name}_${i}`} className="text-uppercase d-inline"><Badge className={`pokemon-type-${pokemonType.type.name}`} variant="dark">{pokemonType.type.name}</Badge>{' '}</h6>
                                     )
                                 }</div>
                                 <Button onClick={() => setModalShow(true)} variant="dark">Ver mas</Button>

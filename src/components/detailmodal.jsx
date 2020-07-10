bd244e578be65e838cc377b3c48cd96f6161f913
unknown <hpcarg@gmail.com>
2020-07-09 22:08:13 -0300

    evolutions-table-w-images

--- src/components/detailmodal.jsx
+++ src/components/detailmodal.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Modal, Col, Row, Badge } from "react-bootstrap";
+import { Modal, Col, Row, Badge, Table } from "react-bootstrap";
 
 const DetailModal = (props) => {
-    const pokemon = props.pokemon_data || undefined;
+    const pokemon = props.pokemon_data;
     return (
       <Modal
         {...props}
@@ -18,30 +18,47 @@ const DetailModal = (props) => {
         <Modal.Body className="bg-dark text-white">
             {
             pokemon.stats &&
-                <Row>
-                    <Col xs={3}>
-                        <p>N° {pokemon.id}</p>
-                        {
-                          pokemon.types.map(pokemonType => 
-                                  <h5 key={`${pokemonType.type.name}_badge`} className="text-uppercase d-inline"><Badge variant="dark">{pokemonType.type.name}</Badge></h5>
-                          )
-                        }
-                    </Col>
-                    <Col xs={3}>
+              <div>
+                  <Row>
+                      <Col xs={3}>
+                          <p>N° {pokemon.id}</p>
                           {
-                            pokemon.stats.map(pokemonStat => 
-                              <div key={`stat_${pokemonStat.stat.name}`}><span className="text-capitalize" key={`stat_${pokemonStat.stat.name}`}>{pokemonStat.stat.name}: {pokemonStat.base_stat}</span><br/></div>
+                            pokemon.types.map(pokemonType => 
+                                    <h5 key={`${pokemonType.type.name}_badge`} className="text-uppercase d-inline"><Badge variant="dark">{pokemonType.type.name}</Badge></h5>
                             )
                           }
-                    </Col>
-                    <Col xs={3}>
+                      </Col>
+                      <Col xs={3}>
+                            {
+                              pokemon.stats.map(pokemonStat => 
+                                <div key={`stat_${pokemonStat.stat.name}`}><span className="text-capitalize" key={`stat_${pokemonStat.stat.name}`}>{pokemonStat.stat.name}: {pokemonStat.base_stat}</span><br/></div>
+                              )
+                            }
+                      </Col>
+                  </Row>
+                  <Row>
+                    <div className="d-flex">
                       {
                         pokemon.evolutions && pokemon.evolutions.map((pokemonEvolution, i) => 
-                            <div key={`${pokemonEvolution.id}_${i}`}><span key={`${pokemonEvolution.id}_${i}`} className="text-white text-capitalize">{pokemonEvolution.name}</span><br/></div>
+                            <div key={`${pokemonEvolution.id}_${i}`}>
+                              <Table>
+                                <thead>
+                                  <tr>
+                                    <th key={`${pokemonEvolution.id}_${i}`} className="text-white text-capitalize border-0">{pokemonEvolution.name}</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td className="border-0"><img src={`${pokemonEvolution.sprites.front_default}`} alt={`evolution_image_${pokemonEvolution.id}`} /></td>
+                                  </tr>
+                                </tbody>
+                              </Table>
+                            </div>
                           )
                       }
-                    </Col>
-                </Row>
+                    </div>
+                  </Row>
+              </div>
             }
         </Modal.Body>
       </Modal>

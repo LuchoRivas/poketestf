013ecf07d7df527381f95fded279ee4bb10a12bb
unknown <hpcarg@gmail.com>
2020-07-09 23:48:47 -0300

    pokemon-por-tipo-servicio

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -3,6 +3,11 @@ import { envConfig } from "../config";
 const apiEndpoint = `${envConfig.apiUrl}/pokemon/`;
 
 export async function getPokemonByName(pokemonName) {
-  const { data: pokemon } = await Axios.get(`${apiEndpoint}pokemonSearch/` + pokemonName + "");
+  const { data: pokemon } = await Axios.get(`${apiEndpoint}pokemonSearch/${pokemonName}`);
+  return pokemon;
+}
+
+export async function getPokemosnByType(type) {
+  const { data: pokemon } = await Axios.get(`${apiEndpoint}typeSearch/${type}`);
   return pokemon;
 }

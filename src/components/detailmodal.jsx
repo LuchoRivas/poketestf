d2078ad4ea84866ff75c5c67a8a51ccaff0f20cb
unknown <hpcarg@gmail.com>
2020-07-11 02:18:00 -0300

    carousel-y-nombre-de-prop

--- src/components/detailmodal.jsx
+++ src/components/detailmodal.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Modal, Col, Row, Badge, Table } from "react-bootstrap";
+import DescriptionCarousel from "./descriptionCarousel";
 
 const DetailModal = (props) => {
-    const pokemon = props.pokemon_data;
-    return (
+    const { pokemon_data } = props;
+      return (
       <Modal
         {...props}
         size="lg"
@@ -12,34 +13,41 @@ const DetailModal = (props) => {
       >
         <Modal.Header className="bg-dark text-white" closeButton>
           <Modal.Title id="contained-modal-title-vcenter" className="text-capitalize">
-            {pokemon.name}
+            {pokemon_data.name}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="bg-dark text-white">
             {
-            pokemon.stats &&
+            pokemon_data.stats &&
               <div>
                   <Row>
                       <Col xs={3}>
-                          <p>N° {pokemon.id}</p>
+                          <p>N° {pokemon_data.id}</p>
                           {
-                            pokemon.types.map(pokemonType => 
+                            pokemon_data.types.map(pokemonType => 
                                     <h5 key={`${pokemonType.type.name}_badge`} className="text-uppercase d-inline"><Badge variant="dark">{pokemonType.type.name}</Badge></h5>
                             )
                           }
                       </Col>
                       <Col xs={3}>
                             {
-                              pokemon.stats.map(pokemonStat => 
+                              pokemon_data.stats.map(pokemonStat => 
                                 <div key={`stat_${pokemonStat.stat.name}`}><span className="text-capitalize" key={`stat_${pokemonStat.stat.name}`}>{pokemonStat.stat.name}: {pokemonStat.base_stat}</span><br/></div>
                               )
                             }
                       </Col>
+                      <Col xs={6}>
+                        {/* descripciones */}
+                        <DescriptionCarousel
+                          items={pokemon_data.species}
+                          captionText={'flavor_text'}
+                        ></DescriptionCarousel>
+                      </Col>
                   </Row>
                   <Row>
-                    <div className="d-flex">
+                    <div className="d-flex col-12">
                       {
-                        pokemon.evolutions && pokemon.evolutions.map((pokemonEvolution, i) => 
+                        pokemon_data.evolutions && pokemon_data.evolutions.map((pokemonEvolution, i) => 
                             <div key={`${pokemonEvolution.id}_${i}`}>
                               <Table>
                                 <thead>
